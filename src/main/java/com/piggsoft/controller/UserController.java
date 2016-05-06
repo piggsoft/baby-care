fe8a5d8c7b319adf56663423ec561d92a4773522
Sean <120656194@qq.com>
2016-05-06 16:12:42 +0800

    试试

--- src/main/java/com/piggsoft/controller/UserController.java
+++ src/main/java/com/piggsoft/controller/UserController.java
@@ -1,7 +1,11 @@
 package com.piggsoft.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by zxshen2 on 2016/5/6.
  */
+@RestController
 public class UserController {
+
 }
