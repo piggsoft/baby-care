81e867e4beaaf4dd432e642bb81fe7e29c208c2a
piggsoft <piggsoft@163.com>
2016-05-09 19:22:13 +0800

    修改test case

--- src/test/java/com/piggsoft/service/impl/UserServiceTest.java
+++ src/test/java/com/piggsoft/service/impl/UserServiceTest.java
@@ -3,7 +3,9 @@ package com.piggsoft.service.impl;
 import com.piggsoft.DemoApplicationTests;
 import com.piggsoft.model.User;
 import com.piggsoft.service.UserService;
+import org.junit.Assert;
 import org.junit.Test;
+import org.springframework.beans.factory.FactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,7 +25,8 @@ public class UserServiceTest extends DemoApplicationTests {
 
     @Test
     public void test() {
-        userService.existPhone("13600000000");
+        boolean flag = userService.existPhone("13600000000");
+        Assert.assertTrue(flag);
     }
 
     @Test
@@ -31,7 +34,7 @@ public class UserServiceTest extends DemoApplicationTests {
     public void testRegister() {
         User user = new User();
         user.setName("haha");
-        user.setPhone("13600000000");
+        user.setPhone("13600000001");
         user.setPassword("123456");
         user.setCreateTm(new Date());
         userService.register(user);
